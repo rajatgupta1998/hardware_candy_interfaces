LineageOS/android_hardware_lineage_interfaces

livedisplay: Move Color helper functions to anonymous namespace
* modePointerToObj and invalidDisplayMode do not need to be part of the
  class definition, they just need to be available.

Change-Id: Id65dd94e06404d82466d349796a52f311dff36ea

livedisplay/1.0/default/src/Color.cpp
@@ -26,6 +26,25 @@
 #error "Color backend undefined!"
 #endif
 
+namespace {
+
+using vendor::lineage::livedisplay::V1_0::DisplayMode;
+using vendor::lineage::livedisplay::V1_0::implementation::disp_mode;
+
+DisplayMode modePointerToObj(android::sp<disp_mode> mode) {
+    DisplayMode m;
+    m.id = mode->id;
+    m.name = mode->name;
+    return m;
+}
+
+DisplayMode invalidDisplayMode() {
+    DisplayMode mode;
+    mode.id = -1;
+    return mode;
+}
+} // anonymous namespace
+
 namespace vendor {
 namespace lineage {
 namespace livedisplay {
@@ -96,19 +115,6 @@ sp<Color> Color::getInstance() {
         sInstance = new Color();
     }
     return sInstance;
-}
-
-DisplayMode Color::modePointerToObj(sp<disp_mode> mode) {
-    DisplayMode m;
-    m.id = mode->id;
-    m.name = mode->name;
-    return m;
-}
-
-DisplayMode Color::invalidDisplayMode() {
-    DisplayMode mode;
-    mode.id = -1;
-    return mode;
 }
 
 Return<Features> Color::getSupportedFeatures() {

rajatgupta1998/hardware_candy_interfaces

livedisplay/1.0/default/src/Color.cpp
@@ -26,6 +26,25 @@
 #error "Color backend undefined!"
 #endif
 
+namespace {
+
+using vendor::candy::livedisplay::V1_0::DisplayMode;
+using vendor::candy::livedisplay::V1_0::implementation::disp_mode;
+
+DisplayMode modePointerToObj(android::sp<disp_mode> mode) {
+    DisplayMode m;
+    m.id = mode->id;
+    m.name = mode->name;
+    return m;
+}
+
+DisplayMode invalidDisplayMode() {
+    DisplayMode mode;
+    mode.id = -1;
+    return mode;
+}
+}  // anonymous namespace
+
 namespace vendor {
 namespace candy {
 namespace livedisplay {
@@ -96,19 +115,6 @@ sp<Color> Color::getInstance() {
         sInstance = new Color();
     }
     return sInstance;
-}
-
-DisplayMode Color::modePointerToObj(sp<disp_mode> mode) {
-    DisplayMode m;
-    m.id = mode->id;
-    m.name = mode->name;
-    return m;
-}
-
-DisplayMode Color::invalidDisplayMode() {
-    DisplayMode mode;
-    mode.id = -1;
-    return mode;
 }
 
 Return<Features> Color::getSupportedFeatures() {